7f3a89989946ff99733d8126cb6506284335cf07
Karel Peeters <karel.peeters.leuven@gmail.com>
2025-11-09 15:07:45 +0100

    Re-join syscall entry and exit.

--- src/main.rs
+++ src/main.rs
@@ -36,9 +36,17 @@ fn main() {
     let child = cmd.spawn().expect("failed to spawn child");
     let child_pid = Pid::from_raw(child.id() as i32);
 
-    let ptrace_options =
-        ptrace::Options::PTRACE_O_TRACESYSGOOD | ptrace::Options::PTRACE_O_EXITKILL;
+    // options:
+    // * PTRACE_O_TRACESYSGOOD: add mask to syscall stops, allows parsing WaitStatus::PtraceSyscall
+    // * PTRACE_O_EXITKILL: kill traced process if tracer exits to avoid orphaned processes
+    // * PTRACE_O_TRACE*: trace children through fork syscalls?
+    let ptrace_options = ptrace::Options::PTRACE_O_TRACESYSGOOD | ptrace::Options::PTRACE_O_EXITKILL;
+    // | ptrace::Options::PTRACE_O_TRACEFORK
+    // | ptrace::Options::PTRACE_O_TRACEVFORK
+    // | ptrace::Options::PTRACE_O_TRACECLONE;
     ptrace::setoptions(child_pid, ptrace_options).expect("failed to set ptrace options");
+
+    let mut partial_syscall = None;
 
     loop {
         ptrace::syscall(child_pid, None).expect("failed ptrace::syscall");
@@ -50,9 +58,6 @@ fn main() {
                     break;
                 }
             }
-            WaitStatus::Signaled(_, _, _) => todo!(),
-            WaitStatus::Stopped(_, _) => todo!(),
-            WaitStatus::PtraceEvent(_, _, _) => todo!(),
             WaitStatus::PtraceSyscall(pid) => {
                 let info = ptrace_syscall_info(pid).expect("failed ptrace::syscall_info");
 
@@ -60,40 +65,67 @@ fn main() {
 
                 match info.op {
                     libc::PTRACE_SYSCALL_INFO_ENTRY => {
+                        assert!(partial_syscall.is_none());
+
                         let entry = unsafe { &info.u.entry };
                         let nr = Sysno::new(entry.nr as usize);
 
-                        if let Some(nr) = nr {
+                        let next_partial_syscall = if let Some(nr) = nr {
                             match nr {
-                                // fork-like
-                                Sysno::clone => todo!(),
-                                Sysno::fork => todo!(),
-                                Sysno::vfork => todo!(),
-                                Sysno::clone3 => todo!(),
-                                // exec-like
-                                Sysno::execve => todo!(),
-                                Sysno::execveat => todo!(),
-                                // exit-like
-                                Sysno::exit => todo!(),
-                                Sysno::exit_group => todo!(),
-                                // ignore everything else
-                                _ => {}
+                                Sysno::clone | Sysno::fork | Sysno::vfork | Sysno::clone3 => SyscallEntry::Fork,
+                                Sysno::execve | Sysno::execveat => SyscallEntry::Exec,
+                                Sysno::exit | Sysno::exit_group => SyscallEntry::Exit,
+                                _ => SyscallEntry::Ignore,
+                            }
+                        } else {
+                            // ignore unknown syscalls
+                            SyscallEntry::Ignore
+                        };
+
+                        println!("  entry {:?}", nr);
+
+                        partial_syscall = Some(next_partial_syscall);
+                    }
+                    libc::PTRACE_SYSCALL_INFO_EXIT => {
+                        let partial = partial_syscall.take().unwrap();
+
+                        let entry = unsafe { &info.u.exit };
+
+                        match partial {
+                            SyscallEntry::Ignore => {}
+                            SyscallEntry::Fork => {
+                                // TODO record forked process pid
+                            }
+                            SyscallEntry::Exec => {
+                                // TODO record new process info (only if successful!)
+                            }
+                            SyscallEntry::Exit => {
+                                // TODO record process exit
                             }
                         }
 
-                        println!("  entry {:?}", nr);
-                    }
-                    libc::PTRACE_SYSCALL_INFO_EXIT => {
-                        let entry = unsafe { &info.u.exit };
                         println!("  exit {}", entry.sval);
                     }
                     _ => {}
                 }
             }
+            // ignore these, we only care about (some) syscalls
+            WaitStatus::Signaled(_, _, _) => todo!(),
+            WaitStatus::Stopped(_, _) => {}
+            WaitStatus::PtraceEvent(pid, signal, extra) => {
+                println!("ptrace event: pid={pid} signal={signal:?} extra={extra}");
+            }
             WaitStatus::Continued(_) => todo!(),
             WaitStatus::StillAlive => todo!(),
         }
     }
+}
+
+enum SyscallEntry {
+    Ignore,
+    Fork,
+    Exec,
+    Exit,
 }
 
 /// Fixed version of ptrace::syscall_info.

0710d1a193426fd4c21d11a3317747828b6030e0
Karel Peeters <karel.peeters.leuven@gmail.com>
2025-11-11 00:30:15 +0100

    Move thread bodies to separate functions.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #![cfg(unix)]
 
 use clap::Parser;
-use crossbeam::channel::{RecvError, SendError, TryRecvError};
+use crossbeam::channel::{Receiver, RecvError, SendError, Sender, TryRecvError};
 use std::ffi::CString;
 use std::ops::ControlFlow;
 use std::process::ExitCode;
@@ -10,7 +10,7 @@ use std::sync::Arc;
 use wtf::gui::{main_gui, DataToGui, GuiHandle};
 use wtf::layout::place_processes;
 use wtf::record::Recording;
-use wtf::trace::{record_trace, TraceEvent};
+use wtf::trace::{record_trace, SpawnFailed, TraceEvent};
 
 #[derive(Debug, Parser)]
 struct Args {
@@ -31,70 +31,13 @@ fn main() -> ExitCode {
     // TODO result handling would also be nicer with split, then we get the result in the first half already
     let handle_tracer = {
         let stopped = stopped.clone();
-        std::thread::spawn(move || unsafe {
-            let record_result = record_trace(&args.command[0], &args.command[0..], |event| {
-                if stopped.load(Ordering::Relaxed) {
-                    return ControlFlow::Break(());
-                }
-
-                match event_tx.send(event) {
-                    Ok(()) => ControlFlow::Continue(()),
-                    Err(SendError(_)) => ControlFlow::Break(()),
-                }
-            });
-            drop(event_tx);
-
-            if record_result.is_err() {
-                stopped.store(true, Ordering::Relaxed);
-            }
-
-            record_result
-        })
+        std::thread::spawn(move || unsafe { thread_tracer(&args, stopped, event_tx) })
     };
 
     // spawn collector thread
     let handle_collector = {
         let stopped = stopped.clone();
-        std::thread::spawn(move || {
-            let gui_handle = match gui_handle_rx.recv() {
-                Ok(handle) => handle,
-                Err(RecvError) => return,
-            };
-            drop(gui_handle_rx);
-
-            let mut recording = Recording::new();
-
-            loop {
-                if stopped.load(Ordering::Relaxed) {
-                    break;
-                }
-
-                // wait for next event
-                match event_rx.recv() {
-                    Ok(event) => recording.report(event),
-                    Err(RecvError) => break,
-                }
-                // batch collect all available events
-                match event_rx.try_recv() {
-                    Ok(event) => recording.report(event),
-                    Err(TryRecvError::Empty) => {}
-                    Err(TryRecvError::Disconnected) => break,
-                }
-
-                // compute a new mapping
-                // TODO send event to gui?
-                // TODO make thread inclusion configurable from the GUI
-                // TODO avoid deep cloning here?
-                let placed = place_processes(&recording, false);
-                let data = DataToGui {
-                    recording: recording.clone(),
-                    placed,
-                };
-
-                *gui_handle.data_to_gui.lock().unwrap() = Some(data);
-                gui_handle.ctx.request_repaint();
-            }
-        })
+        std::thread::spawn(move || thread_collector(stopped, event_rx, gui_handle_rx))
     };
 
     // start gui (egui wants this to be on the main thread)
@@ -113,4 +56,70 @@ fn main() -> ExitCode {
     }
 
     ExitCode::SUCCESS
+}
+
+unsafe fn thread_tracer(
+    args: &Args,
+    stopped: Arc<AtomicBool>,
+    event_tx: Sender<TraceEvent>,
+) -> Result<(), SpawnFailed> {
+    let record_result = unsafe {
+        record_trace(&args.command[0], &args.command[0..], |event| {
+            if stopped.load(Ordering::Relaxed) {
+                return ControlFlow::Break(());
+            }
+
+            match event_tx.send(event) {
+                Ok(()) => ControlFlow::Continue(()),
+                Err(SendError(_)) => ControlFlow::Break(()),
+            }
+        })
+    };
+    drop(event_tx);
+
+    if record_result.is_err() {
+        stopped.store(true, Ordering::Relaxed);
+    }
+
+    record_result
+}
+
+fn thread_collector(stopped: Arc<AtomicBool>, event_rx: Receiver<TraceEvent>, gui_handle_rx: Receiver<GuiHandle>) {
+    let gui_handle = match gui_handle_rx.recv() {
+        Ok(handle) => handle,
+        Err(RecvError) => return,
+    };
+    drop(gui_handle_rx);
+
+    let mut recording = Recording::new();
+
+    loop {
+        if stopped.load(Ordering::Relaxed) {
+            break;
+        }
+
+        // wait for next event
+        match event_rx.recv() {
+            Ok(event) => recording.report(event),
+            Err(RecvError) => break,
+        }
+        // batch collect all available events
+        match event_rx.try_recv() {
+            Ok(event) => recording.report(event),
+            Err(TryRecvError::Empty) => {}
+            Err(TryRecvError::Disconnected) => break,
+        }
+
+        // compute a new mapping
+        // TODO make thread inclusion configurable from the GUI
+        // TODO avoid deep cloning here?
+        let placed = place_processes(&recording, false);
+        let data = DataToGui {
+            recording: recording.clone(),
+            placed,
+        };
+
+        *gui_handle.data_to_gui.lock().unwrap() = Some(data);
+        gui_handle.ctx.request_repaint();
+    }
 }

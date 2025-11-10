2f0bd1751dee8f3ab61e7ca069d5e44b57628837
Karel Peeters <karel.peeters.leuven@gmail.com>
2025-11-11 00:50:53 +0100

    Handle process hovering.

--- src/gui.rs
+++ src/gui.rs
@@ -31,20 +31,16 @@ pub fn main_gui(channel: Sender<GuiHandle>) -> eframe::Result<()> {
         "wtf",
         native_options,
         Box::new(|ctx| {
-            let data_to_gui = Arc::new(Mutex::new(None));
+            let app = App::new();
+
             let interact = GuiHandle {
-                data_to_gui: data_to_gui.clone(),
+                data_to_gui: app.data_to_gui.clone(),
                 ctx: ctx.egui_ctx.clone(),
             };
             let _ = channel.send(interact);
             drop(channel);
 
-            Ok(Box::new(App {
-                data_to_gui,
-                data: None,
-                zoom_linear: 0.0,
-                selected_pid: None,
-            }))
+            Ok(Box::new(app))
         }),
     )
 }
@@ -54,7 +50,9 @@ struct App {
     data: Option<DataToGui>,
 
     zoom_linear: f32,
+
     selected_pid: Option<Pid>,
+    hovered_pid: Option<Pid>,
 }
 
 impl eframe::App for App {
@@ -62,13 +60,6 @@ impl eframe::App for App {
         // try getting new data
         if let Some(new_data) = self.data_to_gui.lock().unwrap().take() {
             self.data = Some(new_data);
-        }
-
-        if self.selected_pid.is_none()
-            && let Some(data) = &self.data
-            && let Some(root_pid) = data.recording.root_pid
-        {
-            self.selected_pid = Some(root_pid);
         }
 
         SidePanel::right("side_panel").show(ctx, |ui| {
@@ -91,8 +82,13 @@ impl eframe::App for App {
                         return;
                     };
 
-                    if let Some(clicked_pid) = self.show_timeline(ui, recording, root_placed) {
-                        self.selected_pid = Some(clicked_pid);
+                    if let Some(pointer_pid_info) = self.show_timeline(ui, recording, root_placed) {
+                        self.hovered_pid = Some(pointer_pid_info.pid);
+                        if pointer_pid_info.clicked {
+                            self.selected_pid = Some(pointer_pid_info.pid);
+                        }
+                    } else {
+                        self.hovered_pid = None;
                     }
 
                     // handle zoom events
@@ -107,7 +103,22 @@ impl eframe::App for App {
     }
 }
 
+struct PointerPidInfo {
+    pid: Pid,
+    clicked: bool,
+}
+
 impl App {
+    fn new() -> Self {
+        Self {
+            data_to_gui: Arc::new(Mutex::new(None)),
+            data: None,
+            zoom_linear: 0.0,
+            selected_pid: None,
+            hovered_pid: None,
+        }
+    }
+
     fn proc_rect(&self, row: usize, height: usize, time: RangeInclusive<f32>) -> Rect {
         let h = 20.0;
         let w = 200.0 * (self.zoom_linear / 100.0).exp();
@@ -118,7 +129,12 @@ impl App {
         }
     }
 
-    fn show_timeline(&self, ui: &mut egui::Ui, recording: &Recording, root_placed: &PlacedProcess) -> Option<Pid> {
+    fn show_timeline(
+        &self,
+        ui: &mut egui::Ui,
+        recording: &Recording,
+        root_placed: &PlacedProcess,
+    ) -> Option<PointerPidInfo> {
         // first pass: compute bounding box
         let mut bounding_box = Rect::NOTHING;
         root_placed.visit(&mut |placed, row| {
@@ -134,7 +150,7 @@ impl App {
         // TODO keep animating this while the process is still running?
         let text_color = ui.visuals().text_color();
         let time_bound_end = *root_placed.time_bound.end();
-        let mut last_clicked_pid = None;
+        let mut pointer_pid_info = None;
 
         root_placed.visit(&mut |placed, row| {
             let proc = recording.processes.get(&placed.pid).unwrap();
@@ -147,8 +163,11 @@ impl App {
                 .translate(offset);
 
             let pointer_in_rect = ui.rect_contains_pointer(proc_rect_full);
-            if pointer_in_rect && response.clicked_by(PointerButton::Primary) {
-                last_clicked_pid = Some(proc.pid);
+            if pointer_in_rect {
+                pointer_pid_info = Some(PointerPidInfo {
+                    pid: proc.pid,
+                    clicked: response.clicked_by(PointerButton::Primary),
+                });
             }
 
             let stroke = if pointer_in_rect || self.selected_pid == Some(proc.pid) {
@@ -174,51 +193,59 @@ impl App {
             }
         });
 
-        last_clicked_pid
+        pointer_pid_info
     }
 
     fn selected_pid_info(&self) -> String {
+        // figure out which pid to show info for
+        let pid = self
+            .hovered_pid
+            .or(self.selected_pid)
+            .or_else(|| self.data.as_ref().and_then(|d| d.recording.root_pid));
+        let Some(pid) = pid else {
+            return "No process selected".to_owned();
+        };
+
+        // render info to string
+        const I: &str = "    ";
+
         let mut text = String::new();
-        if let Some(selected_pid) = self.selected_pid {
-            const I: &str = "    ";
-            swriteln!(text, "Selected process:");
-            swriteln!(text, "{I}pid: {}", selected_pid);
+        swriteln!(text, "Selected process:");
+        swriteln!(text, "{I}pid: {}", pid);
 
-            if let Some(data) = &self.data {
-                if let Some(info) = data.recording.processes.get(&selected_pid) {
-                    let mut child_count_processes = 0;
-                    let mut child_count_threads = 0;
-                    for &(kind, _) in &info.children {
-                        match kind {
-                            ProcessKind::Process => child_count_processes += 1,
-                            ProcessKind::Thread => child_count_threads += 1,
-                        }
+        if let Some(data) = &self.data {
+            if let Some(info) = data.recording.processes.get(&pid) {
+                let mut child_count_processes = 0;
+                let mut child_count_threads = 0;
+                for &(kind, _) in &info.children {
+                    match kind {
+                        ProcessKind::Process => child_count_processes += 1,
+                        ProcessKind::Thread => child_count_threads += 1,
                     }
+                }
 
-                    swriteln!(text, "{I}time_start: {}", info.time_start);
-                    swriteln!(text, "{I}time_end: {:?}", info.time_end);
-                    let duration = info.time_end.map(|time_end| time_end - info.time_start);
-                    swriteln!(text, "{I}duration: {:?}", duration);
+                swriteln!(text, "{I}time_start: {}", info.time_start);
+                swriteln!(text, "{I}time_end: {:?}", info.time_end);
+                let duration = info.time_end.map(|time_end| time_end - info.time_start);
+                swriteln!(text, "{I}duration: {:?}", duration);
 
-                    swriteln!(text, "{I}children: {}", child_count_processes);
-                    swriteln!(text, "{I}threads: {}", child_count_threads);
+                swriteln!(text, "{I}children: {}", child_count_processes);
+                swriteln!(text, "{I}threads: {}", child_count_threads);
 
-                    swriteln!(text, "execs: {}", info.execs.len());
+                swriteln!(text, "execs: {}", info.execs.len());
 
-                    for exec in &info.execs {
-                        swriteln!(text, "{I}{I}time: {}", exec.time);
-                        swriteln!(text, "{I}{I}path: {}", exec.path);
+                for exec in &info.execs {
+                    swriteln!(text, "{I}{I}time: {}", exec.time);
+                    swriteln!(text, "{I}{I}path: {}", exec.path);
 
-                        swriteln!(text, "{I}{I}argv:");
-                        for arg in &exec.argv {
-                            swriteln!(text, "{I}{I}{I}{}", arg);
-                        }
+                    swriteln!(text, "{I}{I}argv:");
+                    for arg in &exec.argv {
+                        swriteln!(text, "{I}{I}{I}{}", arg);
                     }
                 }
             }
-        } else {
-            swriteln!(text, "No process selected");
-        }
+        };
+
         text
     }
 }

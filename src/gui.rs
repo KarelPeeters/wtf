36e52e20e5d9c7f40ffa6a3fade1df1cfb6ff371
Karel Peeters <karel.peeters.leuven@gmail.com>
2025-11-11 01:36:04 +0100

    Improve coloring somewhat.

--- src/gui.rs
+++ src/gui.rs
@@ -5,8 +5,10 @@ use crossbeam::channel::Sender;
 use eframe::emath::{Pos2, Rect};
 use eframe::epaint::{Color32, CornerRadiusF32, FontId, Stroke, StrokeKind};
 use eframe::Frame;
+use egui::ecolor::Hsva;
 use egui::scroll_area::{ScrollBarVisibility, ScrollSource};
 use egui::{CentralPanel, Context, PointerButton, ScrollArea, Sense, SidePanel};
+use egui_theme_switch::global_theme_switch;
 use nix::unistd::Pid;
 use std::ops::RangeInclusive;
 use std::sync::{Arc, Mutex};
@@ -51,11 +53,26 @@ struct App {
     data_to_gui: Arc<Mutex<Option<DataToGui>>>,
     data: Option<DataToGui>,
 
+    color_settings: ColorSettings,
     zoom_linear: f32,
     show_threads: bool,
 
     selected_pid: Option<Pid>,
     hovered_pid: Option<Pid>,
+}
+
+impl App {
+    fn new() -> Self {
+        Self {
+            data_to_gui: Arc::new(Mutex::new(None)),
+            data: None,
+            color_settings: ColorSettings::new(),
+            zoom_linear: 0.0,
+            show_threads: false,
+            selected_pid: None,
+            hovered_pid: None,
+        }
+    }
 }
 
 impl eframe::App for App {
@@ -68,8 +85,28 @@ impl eframe::App for App {
         SidePanel::right("side_panel").show(ctx, |ui| {
             ui.take_available_space();
 
-            ui.checkbox(&mut self.show_threads, "show threads");
+            ui.heading("Settings");
+            global_theme_switch(ui);
+            ui.checkbox(&mut self.show_threads, "Show threads");
 
+            ui.separator();
+            ui.heading("Colors");
+            ui.add(egui::Slider::new(&mut self.color_settings.hue_sat, 0.0..=1.0).text("Hue saturation"));
+            ui.add(egui::Slider::new(&mut self.color_settings.val_header_dark, 0.0..=1.0).text("Value Header (dark)"));
+            ui.add(
+                egui::Slider::new(&mut self.color_settings.val_background_dark, 0.0..=1.0)
+                    .text("Value Background (dark)"),
+            );
+            ui.add(
+                egui::Slider::new(&mut self.color_settings.val_header_light, 0.0..=1.0).text("Value Header (light)"),
+            );
+            ui.add(
+                egui::Slider::new(&mut self.color_settings.val_background_light, 0.0..=1.0)
+                    .text("Value Background (light)"),
+            );
+
+            ui.separator();
+            ui.heading("Process info");
             ui.label(self.selected_pid_info());
         });
 
@@ -124,17 +161,6 @@ struct PointerPidInfo {
 }
 
 impl App {
-    fn new() -> Self {
-        Self {
-            data_to_gui: Arc::new(Mutex::new(None)),
-            data: None,
-            zoom_linear: 0.0,
-            show_threads: false,
-            selected_pid: None,
-            hovered_pid: None,
-        }
-    }
-
     fn proc_rect(&self, row: usize, height: usize, time: RangeInclusive<f32>) -> Rect {
         let h = 20.0;
         let w = 200.0 * (self.zoom_linear / 100.0).exp();
@@ -186,20 +212,32 @@ impl App {
                 });
             }
 
-            let stroke = if pointer_in_rect || self.selected_pid == Some(proc.pid) {
-                Stroke::new(1.0, text_color)
-            } else {
-                Stroke::NONE
-            };
-
-            // TODO better coloring
-            // TODO stroke around all children?
-            let color_scale = placed.depth as f32 / root_placed.max_depth as f32;
-            let color = Color32::from_gray((20.0 + (80.0 * color_scale)) as u8);
-            painter.rect(proc_rect_full, CornerRadiusF32::ZERO, color, stroke, StrokeKind::Inside);
-
             let text = proc.execs.first().map(|exec| exec.path.as_str()).unwrap_or("?");
             let text = text.rsplit_once("/").map(|(_, s)| s).unwrap_or(text);
+
+            let colors = get_process_color(&self.color_settings, ui.visuals().dark_mode, text);
+
+            // TODO separate stoke color, even more consisting than header itself
+            let stroke_color = if pointer_in_rect || self.selected_pid == Some(proc.pid) {
+                text_color
+            } else {
+                colors.header
+            };
+
+            painter.rect(
+                proc_rect_full,
+                CornerRadiusF32::ZERO,
+                colors.background,
+                Stroke::new(1.0, stroke_color),
+                StrokeKind::Inside,
+            );
+            painter.rect(
+                proc_rect_header,
+                CornerRadiusF32::ZERO,
+                colors.header,
+                Stroke::NONE,
+                StrokeKind::Inside,
+            );
 
             let galley = painter.layout_no_wrap(text.to_owned(), FontId::default(), text_color);
 
@@ -264,4 +302,59 @@ impl App {
 
         text
     }
+}
+
+struct ColorSettings {
+    hue_sat: f32,
+
+    val_header_dark: f32,
+    val_background_dark: f32,
+    val_header_light: f32,
+    val_background_light: f32,
+}
+
+struct ProcessColors {
+    header: Color32,
+    background: Color32,
+}
+
+impl ColorSettings {
+    fn new() -> Self {
+        Self {
+            hue_sat: 0.8,
+            val_header_dark: 0.08,
+            val_background_dark: 0.03,
+            val_header_light: 0.6,
+            val_background_light: 0.75,
+        }
+    }
+}
+
+fn get_process_color(settings: &ColorSettings, dark_mode: bool, name: &str) -> ProcessColors {
+    let (hue, sat) = match get_process_hue(name) {
+        Some(hue) => (hue, settings.hue_sat),
+        None => (0.0, 0.0),
+    };
+
+    let (val_header, val_background) = if dark_mode {
+        (settings.val_header_dark, settings.val_background_dark)
+    } else {
+        (settings.val_header_light, settings.val_background_light)
+    };
+
+    ProcessColors {
+        header: Color32::from(Hsva::new(hue, sat, val_header, 1.0)),
+        background: Color32::from(Hsva::new(hue, sat, val_background, 1.0)),
+    }
+}
+
+fn get_process_hue(name: &str) -> Option<f32> {
+    let map: &[(&[&str], f32)] = &[(&["python"], 206.44), (&["rustc", "cargo"], 14.92)];
+
+    for &(list, hue) in map {
+        if list.iter().any(|s| name.contains(s)) {
+            return Some(hue);
+        }
+    }
+    None
 }
